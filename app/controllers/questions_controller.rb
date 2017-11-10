f239684dfd373a5b70f195208d9fd84b587fe440
galactusby <taysvendsen@gmail.com>
2017-11-09 16:22:21 -0800

    adds votes, users, comments, and answers controllers and view files

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,14 @@
 class QuestionsController < ApplicationController
 
   def index
-    @questions = Question.all.sort { |a, b| b.updated_at <=> a.updated_at}
+    @questions = Question.all
   end
 
   def new
+
   end
 
   def show
   end
-  
+
 end
