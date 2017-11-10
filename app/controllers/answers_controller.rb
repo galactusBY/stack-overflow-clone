a7939d5f8c168bf165d8cb8274a2564704f70257
galactusby <taysvendsen@gmail.com>
2017-11-10 10:36:16 -0800

    adds css and images along with controller logic

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,28 @@
 class AnswersController < ApplicationController
 
-  def show
+  def create
+    user = User.find(session[:user_id])
+    if request.xhr?
+      @answer = Answer.new(description: params[:answer])
+      @question = Question.find_by(id: params[:question_id])
+      if @answer.valid?
+        @answer.save
+        user.answers << @answer
+        @question.answers << @answer
+      end
+
+      render :_show, :layout => false
+
+    else
+      @answer = Answer.new(description: params[:answer])
+      @question = Question.find_by(id: params[:question_id])
+      if @answer.valid?
+        @answer.save
+        user.answers << @answer
+        @question.answers << @answer
+      end
+      redirect "/questions/#{@question.id}"
+    end
   end
 
 end
